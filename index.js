b2c19907da518c1557257fd44f02d90a82e56d43
ctgadget <chaance.graves@ctginnovations.io>
2021-11-22 06:48:22 +0000

    First version of name cli.

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+console.log(`
+Chaance T. Graves
+
+Greetings, ladies and gentlemen! 
+
+I’m a creative problem solver eager to implement solutions to many issues foreseen in our modern society. In addition to that, I am also a big advocate for giving back to others who aspire to follow a path like mine.
+
+I currently work at Intel Corporation in the Datacenter AI group as a Platform Applications Engineer. I provide technical support and expertise for Intel server CPUs and server board design development for customers such as Facebook, Google, Amazon Web Services, etc. 
+
+💼 LinkedIn: https://www.linkedin.com/in/chaancegraves/
+😸 Github:   https://github.com/ctg123
+💻 Dev.to:   https://dev.to/ctg123
+👾 Blog:     https://www.chaancegraves.com/
+
+`);
