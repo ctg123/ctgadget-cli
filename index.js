6ae8752e37b34d7da7f1db8fb75ce9dfbaab600c
ctgadget <chaance.graves@ctginnovations.io>
2021-11-23 07:57:23 +0000

    updated with welcome-cli package. to v2.0.0

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
-const clearConsole = require('clear-any-console');
-
-// clears the console.
-clearConsole();
+// Shows welcome text
+welcome({
+    title: pkgJSON.name,
+    tagLine: `Howdy, nice to meet y'all!`,
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true,
+});
 
 console.log(`
 Chaance T. Graves
