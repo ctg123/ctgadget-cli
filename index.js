3102fbff95264de3cc286b222c231070b5506625
ctgadget <chaance.graves@ctginnovations.io>
2021-11-24 15:53:52 +0000

    added colors and italics

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 #!/usr/bin/env node
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk');
+const log = console.log;
+
+//defining chalk features as variables
+const dim = chalk.dim;
+const italic = chalk.italic;
+const linkedinClr = chalk.hex(`#0077b5`).bold.inverse;
+const githubClr = chalk.hex(`#6cc644`).bold.inverse;
+const devtoClr = chalk.hex(`#ffffff`).bold.inverse;
+const blogClr = chalk.hex(`#2962FF`).bold.inverse;
 
 // Shows welcome text
 welcome({
-    title: pkgJSON.name,
+    title: `Chaance T. Graves`,
     tagLine: `Howdy, nice to meet y'all!`,
     description: pkgJSON.description,
     version: pkgJSON.version,
-    bgColor: `#FADC00`,
+    bgColor: `#2962FF`,
     color: `#000000`,
     bold: true,
     clear: true,
 });
 
-console.log(`
-Chaance T. Graves
+log(`${italic(
 
-Greetings, ladies and gentlemen! 
+`Greetings, ladies and gentlemen! 
 
 I’m a creative problem solver eager to implement solutions to many issues foreseen in our modern society. In addition to that, I am also a big advocate for giving back to others who aspire to follow a path like mine.
 
-I currently work at Intel Corporation in the Datacenter AI group as a Platform Applications Engineer. I provide technical support and expertise for Intel server CPUs and server board design development for customers such as Facebook, Google, Amazon Web Services, etc. 
+I currently work at Intel Corporation in the Datacenter AI group as a Platform Applications Engineer. I provide technical support and expertise for Intel server CPUs and server board design development for customers such as Facebook, Google, Amazon Web Services, etc.` 
+)}
 
-💼 LinkedIn: https://www.linkedin.com/in/chaancegraves/
-😸 Github:   https://github.com/ctg123
-💻 Dev.to:   https://dev.to/ctg123
-👾 Blog:     https://www.chaancegraves.com/
+${linkedinClr(` LinkedIn `)} ${dim(`https://www.linkedin.com/in/chaancegraves/`)}
+${githubClr(` Github `)}   ${dim(`https://github.com/ctg123`)}
+${devtoClr(` Dev.to `)}   ${dim(`https://dev.to/ctg123`)}
+${blogClr( ` Blog ` )}     ${dim(`https://www.chaancegraves.com/`)}
 
 `);
